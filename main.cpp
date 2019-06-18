d88fd38f48e660db054499dcc1eeaeb15be3c6b6
Your Name <gyh108wy@163.com>
2019-06-18 23:30:16 +0800

    1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>  
+
+using namespace std;
+
+#define HW "GyhSQL>";
+string DATABASE_NAME = "test";
+string DATABASE_LOCATION = "database/";
+
+int main()
+{
+    string sql;
+    // 初始化输出
+    cout<<"Welcome to GyhSQL\n";
+	cout<<"Server Version 0.1\n";
+	cout<<"\n";
+	cout<<"Try 'help;' for help. Try 'clear;' to clear the current input statement.";
+	cout<<"\n";
+	cout <<HW; 
+    // 定义一些 SQL 语句
+    return 0;
+}
